174b1d35618f09d4c0df0c647ac5479ffc087549
Igor Vernichenko <igorvern@yandex-team.ru>
2022-08-09 00:17:14 +0300

    15-last 16-pop 20-Promise all 106-TrimLeft 108-trim 110-capitalize 191-append-argument

---
+++ 00106-medium-trimleft.ts
@@ -0,0 +1,19 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<TrimLeft<'str'>, 'str'>>,
+  Expect<Equal<TrimLeft<' str'>, 'str'>>,
+  Expect<Equal<TrimLeft<'     str'>, 'str'>>,
+  Expect<Equal<TrimLeft<'     str     '>, 'str     '>>,
+  Expect<Equal<TrimLeft<'   \n\t foo bar '>, 'foo bar '>>,
+  Expect<Equal<TrimLeft<''>, ''>>,
+  Expect<Equal<TrimLeft<' \n\t'>, ''>>,
+]
+
+
+// ============= Your Code Here =============
+type TrimLeft<S extends string> = S extends `${infer Head}${infer Tail}`
+  ? Head extends ' ' | '\n' | '\t'
+    ? TrimLeft<Tail> : `${Head}${Tail}`
+    : S;
---
+++ 00108-medium-trim.ts
@@ -0,0 +1,20 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<Trim<'str'>, 'str'>>,
+  Expect<Equal<Trim<' str'>, 'str'>>,
+  Expect<Equal<Trim<'     str'>, 'str'>>,
+  Expect<Equal<Trim<'str   '>, 'str'>>,
+  Expect<Equal<Trim<'     str     '>, 'str'>>,
+  Expect<Equal<Trim<'   \n\t foo bar \t'>, 'foo bar'>>,
+  Expect<Equal<Trim<''>, ''>>,
+  Expect<Equal<Trim<' \n\t '>, ''>>,
+]
+
+
+// ============= Your Code Here =============
+type Spaces = ' ' | '\n' | '\t';
+type TrimLeft<S extends string> = S extends `${Spaces}${infer Tail}` ? TrimLeft<Tail> : S;
+type TrimRight<S extends string> = S extends `${infer Head}${Spaces}` ? TrimRight<Head> : S;
+type Trim <S extends string> = TrimLeft<TrimRight<S>>;
